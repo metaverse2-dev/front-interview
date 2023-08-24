884b7226f06612c0dac316957f6904f40df67cb9
노아 <ohyoungwoo824@gmail.com>
2023-08-24 20:54:12 +0900

    feat: add QueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/style/globals.css'
-import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/style/global.scss';
+import { AppProps } from "next/app.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient();
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
