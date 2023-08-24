7843d12f82873b737ad2522a60ef6d559c8ebbeb
노아 <ohyoungwoo824@gmail.com>
2023-08-24 20:54:02 +0900

    feat: add common layout

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+import { Header } from "@/component/common/Header";
+
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>{"Todo List"}</title>
+      </Head>
       <body>
+        <Header/>
         <Main />
         <NextScript />
       </body>
